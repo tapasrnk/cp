4bc659c5447d3ee92a14df78e495e38a613f32ff
Tapas Ranjan Nayak <tapasrnk@gmail.com>
2022-06-07 21:41:27 +0530

    adding binary search template

---
+++ binaruSearch.cpp
@@ -0,0 +1,19 @@
+bool ok(int m){
+
+}
+void solve() {
+    //Two invarient l point to possible answer and r point to not possible answer
+    int l = -1, r = -1;
+    while (l + 1 < r) {
+        //Find the middle point
+        int m = l + (r - l) / 2;
+        //Function to check if the middle point is possible answer or not
+        if (ok(m)) {
+            //If possible answer then move l to m
+            l = m;
+        }else {
+            //If not possible answer then move r to m
+            r = m;
+        }
+    }
+}
